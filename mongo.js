81c61b382fbe7042fa8b230a644c86b4a9f89bca
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-06-18 10:42:25 +0300

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://Ratchat1918:${password}@cluster0.ctjwcdz.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set('strictQuery',false);
+
+mongoose.connect(url);
+//tehtävä 3.12
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const phoneName=process.argv[3];
+const phoneNumber=process.argv[4];
+
+const person=new Person({
+  name: phoneName,
+  number:phoneNumber,
+});
+if (process.argv.length < 4) {
+  console.log("phonebook:")
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close();
+  })
+}else{
+  person.save().then(result=>{
+  console.log(`added ${phoneName} ${phoneNumber} to phonebook`);
+  mongoose.connection.close();
+})
+}
